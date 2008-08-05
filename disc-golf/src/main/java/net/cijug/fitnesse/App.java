9480969021b099d68b99fe7a17e16f6bc20debc8
U-ADAMANTIUM\tandersen <timander@gmail.com>
2008-08-05 07:55:56 -0500

    generated app with maven

---
+++ disc-golf/src/main/java/net/cijug/fitnesse/App.java
@@ -0,0 +1,13 @@
+package net.cijug.fitnesse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
