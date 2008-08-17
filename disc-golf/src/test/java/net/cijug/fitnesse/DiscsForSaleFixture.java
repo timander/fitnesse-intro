ce61a7af2502735660b6ec57408ccfe44afb06d5
Tim Andersen <timander@gmail.com>
2008-08-17 10:06:17 -0500

    pushed some store logic from fixture code to main code

--- disc-golf/src/test/java/net/cijug/fitnesse/DiscsForSaleFixture.java
+++ disc-golf/src/test/java/net/cijug/fitnesse/DiscsForSaleFixture.java
@@ -3,24 +3,29 @@ package net.cijug.fitnesse;
 import fitlibrary.DoFixture;
 import net.cijug.discgolf.Disc;
 import net.cijug.discgolf.Player;
-import java.util.HashSet;
+import net.cijug.discgolf.Store;
 import java.util.Set;
 
 
 public class DiscsForSaleFixture extends DoFixture {
 
-    private Set<Disc> discs = new HashSet<Disc>();
+
+    private Store store;
+
+    public DiscsForSaleFixture() {
+        store = new Store();
+    }
 
 
     public Set<Disc> getDiscs() {
-        return new HashSet<Disc>(discs);
+        return store.getDiscs();
     }
 
 
     public boolean purchase(Player player, Set<String> discNames) {
         for (String discName : discNames) {
-            Disc disc = findDisc(discName);
-            discs.remove(disc);
+            Disc disc = store.findDisc(discName);
+            store.sell(disc);
             player.bought(disc);
         }
         return true;
@@ -29,16 +34,8 @@ public class DiscsForSaleFixture extends DoFixture {
 
     public boolean sellsFor(String name, String price) {
         Disc disc = new Disc(name, new Double(price.replaceAll("\\$", "")));
-        return discs.add(disc);
+        return store.addDiscToInventory(disc);
     }
 
 
-    private Disc findDisc(String discName) {
-        for (Disc disc : discs) {
-            if (discName.equals(disc.getName())) {
-                return disc;
-            }
-        }
-        return null;
-    }
 }
